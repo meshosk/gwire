6319e92694a9ef6ed0a7e93d9c1feb3572dd0ce1
Matej Meško <meshosk@gmail.com>
2023-11-30 15:22:56 +0100

    Little changes and draging square test

---
+++ src/components/basic/basic.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const x = ref(0);
+const y = ref(0);
+
+function onMousedown(e) {
+    x = e.offseX;
+    y = e.offseY;
+}                
+
+    
+</script>
+
+<template>
+   <rect x="{{ x }}" y="{{ y }}" width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" @mousedown="onMouseDown" />
+</template>
