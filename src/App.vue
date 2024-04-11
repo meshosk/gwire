63901c23abfb709ec7c7eb61d982df105984f601
meshosk <meshosk@gmail.com>
2024-04-11 22:17:43 +0200

    fixed main element size

--- src/App.vue
+++ src/App.vue
@@ -25,14 +25,13 @@ function load() {
 </script>
 
 <template>
-  <main>
+  <main class="flex-row">
     <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">GWire</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarsExample02">
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
@@ -74,5 +73,8 @@ function load() {
 </template>
 <style scoped lang="scss">
 
-
+main {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
